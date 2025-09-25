ba0644e85ce15ace340f9969438e59b13a24e49c
=Rohit-Verma06 <=rohitav06@gmail.com>
2025-09-26 00:32:38 +0530

    Implement robust NLTK data handling

--- app.py
+++ app.py
@@ -2,58 +2,57 @@ import streamlit as st
 import pickle
 import string
 import nltk
+import os
 
-# We need to import these here to download the data
+# Create a directory for NLTK data
+nltk_data_dir = os.path.join(os.path.dirname(__file__), 'nltk_data')
+if not os.path.exists(nltk_data_dir):
+    os.makedirs(nltk_data_dir)
+
+# Add the custom path to NLTK's data path
+nltk.data.path.append(nltk_data_dir)
+
+# Download necessary NLTK data to the custom directory
+try:
+    nltk.data.find('corpora/stopwords', paths=[nltk_data_dir])
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_dir)
+try:
+    nltk.data.find('tokenizers/punkt', paths=[nltk_data_dir])
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_dir)
+
+# Now we can import these
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-
-# This is a one-time download of the necessary NLTK data
-# It will be skipped if the data is already present
-def download_nltk_data():
-    try:
-        nltk.data.find('corpora/stopwords')
-    except nltk.downloader.DownloadError:
-        nltk.download('stopwords')
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except nltk.downloader.DownloadError:
-        nltk.download('punkt')
-
-download_nltk_data()
-
 
 ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
-
     y = []
     for i in text:
         if i.isalnum():
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
             y.append(i)
-
     text = y[:]
     y.clear()
-
     for i in text:
         y.append(ps.stem(i))
-
     return " ".join(y)
 
+# Load the trained models
 tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
 model = pickle.load(open('model.pkl', 'rb'))
 
+# Streamlit App
 st.title("Email/SMS Spam Classifier")
 st.markdown("Enter the message below to check if it's spam or not.")
-
 input_sms = st.text_area("Enter the message")
 
 if st.button('Predict'):
